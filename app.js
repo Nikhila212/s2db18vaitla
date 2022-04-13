8a883bbdd6ce2910481f9ed60c2e0658399b934a
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-13 14:34:10 -0500

    updated

--- app.js
+++ app.js
@@ -4,40 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-mongoose = require('mongoose');
-var Barbeque = require('./models/barbequeSchema');
 const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,  
+  {useNewUrlParser: true, 
+  useUnifiedTopology: true});
 
+  //Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")});
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var BarbequeRouter = require('./routes/Barbeque');
-var addmodsRouter = require('./routes/addmods');
-var selectorRouter = require('./routes/selector');
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-});
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/Barbeque', BarbequeRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
+var Barbeque = require('./models/barbequeSchema');
 
 // We can seed the collection if needed on server start
 async function recreateDB() {
@@ -67,8 +48,29 @@ async function recreateDB() {
 let reseed = true;
 if (reseed) { recreateDB(); }
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var BarbequeRouter = require('./routes/Barbeque');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/Barbeque', BarbequeRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
