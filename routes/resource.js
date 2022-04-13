d470935d9e17bca36ae6d466b3ed1dc70f729b19
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-13 18:32:04 -0500

    updated

--- routes/resource.js
+++ routes/resource.js
@@ -1,24 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
 // Require controller modules.
 var api_controller = require('../controllers/api');
 var barbeque_controller = require('../controllers/barbeque');
-
 /// API ROUTE ///
 // GET resources base.
 router.get('/resource', api_controller.api);
-
-/// Barbeque ROUTES ///
-// POST request for creating a Barbeque.
-router.post('/barbeque', barbeque_controller.barbeque_create_post);
-// DELETE request to delete Barbeque.
-router.delete('/barbeque/:id', barbeque_controller.barbeque_delete);
-// PUT request to update Barbeque.
-router.put('/barbeque/:id', barbeque_controller.barbeque_update_put);
-// GET request for one Barbeque.
-router.get('/barbeque/:id', barbeque_controller.barbeque_detail);
-// GET request for list of all Barbeque items.
-router.get('/barbeque', barbeque_controller.barbeque_list);
-
+/// barbeque ROUTES ///
+// POST request for creating a barbeque.
+router.post('/resource/barbeque', barbeque_controller.barbeque_create_post);
+// DELETE request to delete barbeque.
+router.delete('/resource/barbeque/:id', barbeque_controller.barbeque_delete);
+// PUT request to update barbeque.
+router.put('/resource/barbeque/:id',
+barbeque_controller.barbeque_update_put);
+// GET request for one barbeque.
+router.get('/resource/barbeque/:id', barbeque_controller.barbeque_detail);
+// GET request for list of all barbeque items.
+router.get('/resource/barbeque', barbeque_controller.barbeque_list);
 module.exports = router;
