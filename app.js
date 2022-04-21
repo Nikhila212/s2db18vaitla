113830be93169e2f8597f1189ede1b10b9d617e7
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-20 21:54:47 -0500

    updated

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 mongoose = require('mongoose');
-var dolphin = require('./models/dolphin');
+var Barbeque = require('./models/Barbeque');
 const connectionString = process.env.MONGO_CON
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dolphinRouter = require('./routes/dolphin');
+var BarbequeRouter = require('./routes/Barbeque');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 var resourceRouter = require('./routes/resource');
@@ -22,18 +22,18 @@ mongoose.connect('mongodb+srv://Chooseyourconnection:padma@cluster0.2h8dj.mongod
 
 var app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirorder_name, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirorder_name, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dolphin', dolphinRouter);
+app.use('/Barbeque', BarbequeRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
@@ -41,23 +41,23 @@ app.use('/resource', resourceRouter);
 // We can seed the collection if needed on server start
 async function recreateDB() {
   // Delete everything
-  await dolphin.deleteMany();
-  let instance1 = new dolphin(
-    { name: "Bottle Nose", age: '23', weight: 60 });
+  await Barbeque.deleteMany();
+  let instance1 = new Barbeque(
+    { order_name: "Chicken", order_quantity: '2', order_price: 60 });
   instance1.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("First object saved")
   });
 
-  let instance2 = new dolphin(
-    { name: "Amazon River", age: '67', weight: 80 });
+  let instance2 = new Barbeque(
+    { order_name: "Fish", order_quantity: '7', order_price: 80 });
   instance2.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("Second object saved")
   });
 
-  let instance3 = new dolphin(
-    { name: "Ganges River", age: '54', weight: 40 });
+  let instance3 = new Barbeque(
+    { order_name: "Prawns", order_quantity: '4', order_price: 40 });
   instance3.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("Third object saved")
@@ -74,10 +74,10 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.messorder_quantity = err.messorder_quantity;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error porder_quantity
   res.status(err.status || 500);
   res.render('error');
 });
